406d86721648551f6d2cf6d0d8f191de410eab13
FelipeCabralViana <felipecv7@gmail.com>
2022-03-07 13:28:55 -0300

    first commit

---
+++ calculadora-de-m-diaaula01/README.markdown
@@ -0,0 +1,5 @@
+# Calculadora de média - Aula01
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/felipecabral-the-vuer/pen/RwjOrgR](https://codepen.io/felipecabral-the-vuer/pen/RwjOrgR).
+
+
---
+++ calculadora-de-m-diaaula01/dist/script.js
@@ -0,0 +1,31 @@
+/*var nome  = "Felipe"
+var notaDoPrimeiroBimestre = 9.2345
+var notaDoSegundoBimestre = 9.654
+var notaDoTerceiroBimestre = 9.876
+var notaDoQuartoBimestre = 9.987
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome)
+console.log("Nota: "+ notaFixada)
+*/
+//Isso é um comentário
+//Revisão
+//Variáveis, Strings, Console.log
+//Concatenação, Utilização do Console
+
+var nota1 = 5
+var nota2 = 5
+var nota3 = 5
+var nota4 = 5
+var notaTotal = nota1 + nota2 + nota3 + nota4
+var notaFinal = notaTotal / 4
+
+console.log(notaFinal)
+
+if (notaFinal > 6){
+  console.log("Você foi aprovado!")
+} else {
+  console.log("Você foi reprovado!")
+}
